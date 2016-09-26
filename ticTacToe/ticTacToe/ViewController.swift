22e52e348cc3f77840c83c3acea7fc08f15ad776
Forrest Zhao <fozhao92@gmail.com>
2016-09-26 10:47:12 -0400

    add hide all buttons func, reset game func

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var nameOfGameLabelo: UILabel!
+  @IBOutlet weak var endGameLabel: UILabel!
+  
+  //top row buttons
+  @IBOutlet weak var topLeftButton: UIButton!
+  @IBOutlet weak var topMiddleButton: UIButton!
+  @IBOutlet weak var topRightButton: UIButton!
+  
+  //middle row buttons
+  @IBOutlet weak var middleLeftButton: UIButton!
+  @IBOutlet weak var middleMiddleButton: UIButton!
+  @IBOutlet weak var middleRightButton: UIButton!
+
+  //bottom row buttons
+  @IBOutlet weak var bottomLeftButton: UIButton!
+  @IBOutlet weak var bottomMiddleButton: UIButton!
+  @IBOutlet weak var bottomRightButton: UIButton!
+  var turnCounter = 1 //odd turn is x, even is o
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+  @IBAction func topLeftButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func topMiddleButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func topRightButtonClicked(_ sender: AnyObject) {
+  }
+  
+  @IBAction func middleLeftButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func middleMiddleButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func middleRightButtonClicked(_ sender: AnyObject) {
+  }
+  
+  @IBAction func bottomLeftButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func bottomMiddleLeftButtonClicked(_ sender: AnyObject) {
+  }
+  @IBAction func bottomRightButtonClicked(_ sender: AnyObject) {
+  }
 
+  func hideAllButtons() {
+    topLeftButton.isHidden = true
+    topMiddleButton.isHidden = true
+    topRightButton.isHidden = true
+    middleLeftButton.isHidden = true
+    middleMiddleButton.isHidden = true
+    middleRightButton.isHidden = true
+    bottomLeftButton.isHidden = true
+    bottomMiddleButton.isHidden = true
+    bottomRightButton.isHidden = true
+  }
+  
+  func resetGame() {
+    hideAllButtons()
+    endGameLabel.isHidden = true
+  }
 }
 
