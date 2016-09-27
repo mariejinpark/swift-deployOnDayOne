869daabfb4e427a23eaa6cbd223de9b9b122885e
Forrest Zhao <fozhao92@gmail.com>
2016-09-27 15:58:14 -0400

    fix ai implementation(still wip), add switch for ai mode

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -29,12 +29,19 @@ class ViewController: UIViewController {
   @IBOutlet weak var bottomMiddleButton: UIButton!
   @IBOutlet weak var bottomRightButton: UIButton!
   
+  @IBOutlet weak var aiSwitch: UISwitch!
+  
+  var buttonGrid: [[UIButton]] = []
+  
   var board: [[String]] = Array(repeating: Array(repeating:"", count:3), count:3)
   var turnCounter = 0 //odd turn is o, even is x
   var isPlayingAI = false //flag for ai mode
   
   override func viewDidLoad() {
         super.viewDidLoad()
+        buttonGrid = [[topLeftButton, topMiddleButton, topRightButton],
+                      [middleLeftButton, middleMiddleButton, middleRightButton],
+                      [bottomLeftButton, bottomMiddleButton, bottomRightButton]]
         resetGame()
     }
 
@@ -48,9 +55,12 @@ class ViewController: UIViewController {
     topLeftButton.setTitle(buttonText, for: .normal)
     board[0][0] = buttonText
     topLeftButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func topMiddleButtonClicked(_ sender: AnyObject) {
@@ -58,9 +68,12 @@ class ViewController: UIViewController {
     topMiddleButton.setTitle(buttonText, for: .normal)
     board[0][1] = buttonText
     topMiddleButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func topRightButtonClicked(_ sender: AnyObject) {
@@ -68,9 +81,12 @@ class ViewController: UIViewController {
     topRightButton.setTitle(buttonText, for: .normal)
     board[0][2] = buttonText
     topRightButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func middleLeftButtonClicked(_ sender: AnyObject) {
@@ -78,9 +94,12 @@ class ViewController: UIViewController {
     middleLeftButton.setTitle(buttonText, for: .normal)
     board[1][0] = buttonText
     middleLeftButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func middleMiddleButtonClicked(_ sender: AnyObject) {
@@ -88,9 +107,12 @@ class ViewController: UIViewController {
     middleMiddleButton.setTitle(buttonText, for: .normal)
     board[1][1] = buttonText
     middleMiddleButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func middleRightButtonClicked(_ sender: AnyObject) {
@@ -98,9 +120,12 @@ class ViewController: UIViewController {
     middleRightButton.setTitle(buttonText, for: .normal)
     board[1][2] = buttonText
     middleRightButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func bottomLeftButtonClicked(_ sender: AnyObject) {
@@ -108,9 +133,12 @@ class ViewController: UIViewController {
     bottomLeftButton.setTitle(buttonText, for: .normal)
     board[2][0] = buttonText
     bottomLeftButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func bottomMiddleLeftButtonClicked(_ sender: AnyObject) {
@@ -118,9 +146,12 @@ class ViewController: UIViewController {
     bottomMiddleButton.setTitle(buttonText, for: .normal)
     board[2][1] = buttonText
     bottomMiddleButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
   
   @IBAction func bottomRightButtonClicked(_ sender: AnyObject) {
@@ -128,14 +159,27 @@ class ViewController: UIViewController {
     bottomRightButton.setTitle(buttonText, for: .normal)
     board[2][2] = buttonText
     bottomRightButton.titleLabel?.isHidden = false;
-    checkIfGameWon()
     turnCounter += 1
     printBoardArr()
+    if isPlayingAI == true {
+      aiPlayerMove()
+    }
+    checkIfGameWon()
   }
 
   @IBAction func startNewGameClicked(_ sender: UIButton) {
     resetGame()
   }
+  
+  @IBAction func aiSwitchOn(_ sender: UISwitch) {
+    if sender.isOn{
+      isPlayingAI = true
+    }
+    else{
+      isPlayingAI = false
+    }
+  }
+  
   
   func hideAllButtons() {
     topLeftButton.titleLabel?.isHidden = true
@@ -149,10 +193,19 @@ class ViewController: UIViewController {
     bottomRightButton.titleLabel?.isHidden = true
   }
   
+  func disableAllButtons() {
+    for inner in buttonGrid{
+      for button in inner {
+        button.isEnabled = false
+      }
+    }
+  }
+  
   func resetGame() {
     endGameLabel.isHidden = true
     hideAllButtons()
     board = Array(repeating: Array(repeating:"", count:3), count:3)
+    aiSwitch.setOn(false, animated: true)
     turnCounter = 0
   }
   
@@ -192,16 +245,28 @@ class ViewController: UIViewController {
     else if board[2][0] != "" && board[2][0] == board[2][1] && board[2][2] == board[2][1]  {
       gameWon(rowIndexValue: board[2][0])
     }
+    //check diagonals
+    else if board[0][0] != "" && board[0][0] == board[1][1] && board[1][1] == board[2][2]  {
+      gameWon(rowIndexValue: board[0][1])
+    }
+    else if board[2][0] != "" && board[2][0] == board[1][1] && board[1][1] == board[0][2]  {
+      gameWon(rowIndexValue: board[2][0])
+    }
   }
   
+  //dumb ai player move
   func aiPlayerMove() {
-    var randomRowIndex = Int(arc4random_uniform(UInt32(board.capacity)))
-    var randomColumnIndex = Int(arc4random_uniform(UInt32(board[0].capacity)))
-    while board[randomRowIndex][randomColumnIndex] == "" && turnCounter < 9 {
-      randomRowIndex = Int(arc4random_uniform(UInt32(board.capacity)))
-      randomColumnIndex = Int(arc4random_uniform(UInt32(board[0].capacity)))
+    var rowIndex = Int(arc4random_uniform(UInt32(board.capacity - 1)))
+    var columnIndex = Int(arc4random_uniform(UInt32(board[0].capacity - 1)))
+    while board[rowIndex][columnIndex] != "" && turnCounter < 9 {
+      rowIndex = Int(arc4random_uniform(UInt32(board.capacity - 1)))
+      columnIndex = Int(arc4random_uniform(UInt32(board[0].capacity - 1)))
     }
-    
+    print("**** random spot = [\(rowIndex), \(columnIndex)] ****")
+    board[rowIndex][columnIndex] = showXorO()
+    buttonGrid[rowIndex][columnIndex].setTitle(showXorO(), for: .normal)
+    buttonGrid[rowIndex][columnIndex].isHidden = false
+    turnCounter += 1
   }
   
   func printBoardArr() {
